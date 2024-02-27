098e1a3ab32b88a37cb33f05ce6be5746ca3a066
Vitaliy Vitanskyy <vitanskyyv@gmail.com>
2024-02-27 11:48:12 -0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { Home } from "./pages/Home/Home";
+import { Movies } from "./pages/Movies/Movies";
+import { Shows } from "./pages/Shows/Shows";
+import { People } from "./pages/People/People";
+import { Favorites } from "./pages/Favorites/Favorites";
 
 function App() {
   
@@ -12,6 +16,10 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/shows" element={<Shows />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </Router>
     </>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
     <div>
-        <div>
-            <nav>
-                <Link>
-                Movie Clips
-                </Link>
-            </nav>
+      <div>
+        <nav className=" hidden md:flex gap-5 justify-start font-bold text-2xl fixed w-full top-0 z-50 bg-gray-200">
+          <Link to={"/"}>Movie Clips</Link>
+          <Link to={"/movies"}>Movies</Link>
+          <Link to={"/shows"}>Tv Shows</Link>
+          <Link to={"/people"}>People</Link>
+          <Link to={"/favorites"}>My Favorites</Link>
+        </nav>
+      </div>
+      <div className="-mr-2 flex md:hidden">
+        <button
+          onClick={handleClick}
+          className="inline-flex items-center justify-center p-2 rounded-md font-bold"
+        >
+          Menu
+        </button>
+      </div>
+      <div className={(nav ? "block" : "hidden") + " md:hidden"}>
+        <div className="flex flex-col gap-3 p-2 font-semibold ">
+          <Link to={"/"}>Movie Clips</Link>
+          <Link to={"/movies"}>Movies</Link>
+          <Link to={"/shows"}>Tv Shows</Link>
+          <Link to={"/people"}>People</Link>
+          <Link to={"/favorites"}>My Favorites</Link>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
