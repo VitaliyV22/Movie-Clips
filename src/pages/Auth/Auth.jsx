4ae8eb270d75e9bf30ddfcfca604347ff6b196f0
Vitaliy Vitanskyy <vitanskyyv@gmail.com>
2024-03-06 15:00:21 -0600

    added functionality to add guest key to local storage

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { useMutation } from "@tanstack/react-query";
 import { mutationLogin } from "./Mutation";
+import { useNavigate } from "react-router-dom";
+
+
 
 export const Auth = () => {
-  
   const { data, mutate } = useMutation({
     mutationKey: ["login"],
     mutationFn: mutationLogin,
-  });
+   
+    
+  }
+  );
 
   //using on Click function to call useMutation which creates a guest session for the user
-  const handleLogin = async () => {
-     await mutate() 
+  const handleLogin = () => {
+    mutate()
+    // localStorage.setItem("guest_id", JSON.stringify(data?.guest_session_id))
+    
   };
 
   return (
--- src/pages/Auth/Mutation.js
+++ src/pages/Auth/Mutation.js
@@ -1,21 +1,25 @@
+import axios from "axios";
+
 export const mutationLogin = async () => {
-    // calling TMBD APi to create a guest session
-  const options = {
+  const APITOKEN = import.meta.env.VITE_API_KEY;
+
+  const guestSess = {
     method: "GET",
+    url: "https://api.themoviedb.org/3/authentication/guest_session/new",
     headers: {
       accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGRlMWJhNDA5MTE3YjU2Y2RiMzk1ZmRjNDI2MGI1MCIsInN1YiI6IjY1ZGUyMDhjYzkyYzVkMDE3YzQ4N2Q1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.99zSdkjP2tu5PJ_bFzkdzFdOvjDxs1ITQ6AII-6NMjA",
+      Authorization: `Bearer ${APITOKEN}`,
     },
   };
 
-  const res = await fetch(
-    "https://api.themoviedb.org/3/authentication/guest_session/new",
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-  console.log(res);
-  return res.json();
+  axios
+    .request(guestSess)
+    .then(function (response) {
+      const guestID = response.data.guest_session_id
+      console.log(guestID);
+      localStorage.setItem("guest_id", guestID);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 };
