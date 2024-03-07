6c5ccc9d0eca20172f54d1c3af33fb6d80bd0243
Vitaliy Vitanskyy <vitanskyyv@gmail.com>
2024-03-07 10:42:46 -0600

    (homepage)working carousel with movie title and image

--- src/components/PopularMovies/PopularMovies.jsx
+++ src/components/PopularMovies/PopularMovies.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import useDataFetch from "../../hooks/useDataFetch";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export const PopularMovies = () => {
   const { data, error, isLoading, refetch } = useDataFetch(
@@ -19,16 +21,50 @@ export const PopularMovies = () => {
   if (!data || !data.results) {
     return <div>No data available.</div>;
   }
+  console.log(data);
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 6,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
 
   return (
     <div>
+      <h1 className="font-bold text-2xl">Trending</h1>
       <div>
-        <h1 className="font-bold text-2xl">Trending</h1>
-        <ul>
+        <Carousel
+          responsive={responsive}
+          autoPlaySpeed={1000}
+          keyBoardControl={true}
+          customTransition="all .5"
+          transitionDuration={500}
+          containerClass="carousel-container"
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+        >
           {data.results.map((movie) => (
-            <li key={movie.id}>{movie.title}</li>
+            <div>
+              <h1 key={movie.id}>{movie.title}</h1>
+              <img src={"https://image.tmdb.org/t/p/w500/" + movie.backdrop_path} alt="" />
+              <p key={movie.release}>{movie.release_date} </p>
+              
+            </div>
           ))}
-        </ul>
+        </Carousel>
       </div>
     </div>
   );
