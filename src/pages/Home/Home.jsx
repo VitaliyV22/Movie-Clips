60db54e5ce05f2b7546ae08f795da8dcdcd50748
Vitaliy Vitanskyy <vitanskyyv@gmail.com>
2024-03-08 10:44:32 -0600

    added button changing color on display for movies or shows

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,19 +6,27 @@ import { PopularShows } from "../../components/PopularShows/PopularShows";
 export const Home = () => {
   const [show, setShow] = useState(false);
   const [movie, setMovie] = useState(true);
+  const [showClicked, setShowClicked] = useState(false);
+  const [movieClicked, setMovieClicked] = useState(true);
 
   const showTVShows = () => {
     // hide movies on click so only shows display
-    setMovie(false)
-    setShow(true)
+    setMovie(false);
+    setShow(true);
+    // changing button color on click
+    setShowClicked(true);
+    setMovieClicked(false)
   };
 
   const showMovies = () => {
     // hide shows on click so only shows movies
-   setMovie(true)
-   setShow(false)
-  }
-  
+    setMovie(true);
+    setShow(false);
+    setMovieClicked(true)
+    setShowClicked(false);
+  };
+
+
 
   return (
     <div>
@@ -26,20 +34,23 @@ export const Home = () => {
       <div className="flex flex-row justify-start items-center p-5">
         <h1 className="font-bold  text-4xl p-5 ">Trending</h1>
         <div className="flex ml-[10px] gap-5 items-center  ">
-          <button onClick={showMovies} className="font-bold text-lg border p-2 rounded-md bg-yellow-500">
+          <button
+            onClick={showMovies}
+            className={`font-bold text-lg border p-2 rounded-md ${movieClicked ? "bg-yellow-500" : "bg-white"}`}
+          >
             Movies
           </button>
           <button
             onClick={showTVShows}
-            className="font-bold text-lg border p-2 rounded-md"
+            className={`font-bold text-lg border  p-2 rounded-md ${showClicked ? "bg-yellow-500" : "bg-white"}`}
           >
             Shows
           </button>
         </div>
       </div>
       <hr />
-      {movie && <PopularMovies /> }
-      
+      {movie && <PopularMovies />}
+
       {show && <PopularShows />}
     </div>
   );
