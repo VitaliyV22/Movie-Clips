2e6cdbf956ee485d1b47fc83afbc52fd257d1d6c
Vitaliy Vitanskyy <vitanskyyv@gmail.com>
2024-03-12 12:33:55 -0500

    added Top Rated Movies & Shows

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,19 +6,23 @@ import { TopRatedMovies } from "../../components/TopRatedMovies/TopRatedMovies";
 import { TopRatedShows } from "../../components/TopRatedShows/TopRatedShows";
 
 export const Home = () => {
-
-  const [isMovieSelected, setIsMovieSelected] = useState(true)
+  const [isMovieSelected, setIsMovieSelected] = useState(true);
+  const [isRatedMovieSelected, setIsRatedMovieSelected] = useState(true);
 
   const showMovies = () => {
-    setIsMovieSelected(true)
-  }
-
-
-  const showTVShows = () => {
-    setIsMovieSelected(false)
+    setIsMovieSelected(true);
   };
 
- 
+  const showTVShows = () => {
+    setIsMovieSelected(false);
+  };
+
+  const showRatedMoviews = () => {
+    setIsRatedMovieSelected(true);
+  };
+  const showRatedShows = () => {
+    setIsRatedMovieSelected(false);
+  };
 
   return (
     <div>
@@ -44,9 +48,31 @@ export const Home = () => {
           </button>
         </div>
       </div>
-      <hr />
+
       {isMovieSelected ? <PopularMovies /> : <PopularShows />}
 
+      <div className="flex flex-row justify-start items-center p-5">
+        <h1 className="font-bold  text-4xl p-5 ">Top Rated</h1>
+        <div className="flex ml-[10px] gap-5 items-center  ">
+          <button
+            onClick={showRatedMoviews}
+            className={`font-bold text-lg border p-2 rounded-md ${
+              isRatedMovieSelected ? "bg-yellow-500" : "bg-white"
+            }`}
+          >
+            Movies
+          </button>
+          <button
+            onClick={showRatedShows}
+            className={`font-bold text-lg border  p-2 rounded-md ${
+              !isRatedMovieSelected ? "bg-yellow-500" : "bg-white"
+            }`}
+          >
+            Shows
+          </button>
+        </div>
+      </div>
+      {isRatedMovieSelected ? <TopRatedMovies /> : <TopRatedShows />}
     </div>
   );
 };
