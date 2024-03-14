0b4b9900edf70852bb884978b5d3604a4a9677f8
Vitaliy Vitanskyy <vitanskyyv@gmail.com>
2024-03-13 22:41:55 -0500

    tweaks to getColor function

--- src/hooks/getColor.js
+++ src/hooks/getColor.js
@@ -1,9 +1,9 @@
 const getColor = (voteAverage) => {
   if (voteAverage > 7) {
-    return "bg-green-500 ";
-  } else if (voteAverage < 7) return "bg-orange-500";
+    return "bg-green-600 ";
+  } else if (voteAverage > 6) return "bg-orange-600";
   else {
-    return "bg-red-500";
+    return "bg-pink-700";
   }
 };
 
